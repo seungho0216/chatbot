74f834a00af04b9f96b049e50f68d75dbfcc72dc
seungho <seungho0216@gmail.com>
2018-08-30 04:12:52 +0000

    1.1.0 lotto

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import os
+import random
 
 app = Flask(__name__)
 
@@ -22,11 +23,22 @@ def message():
     # content라는 key value를 msg에 저장
     msg = request.json["content"]
     
+    if msg == "메뉴":
+        menu = ["20층", "멀캠식당", "찹쌀탕수육", "급식"]
+        return_msg = random.choice(menu)
+    elif msg == "로또":
+        numbers = list(range(1,46))
+        pick = random.sample(numbers, 6)
+        return_msg = str(sorted(pick))
+    else:
+        return_msg = "현재 메뉴만 지원합니다."
+        
+    
     # 카톡에서 명령어 입력시 text 에 있는 문구가 출력됨
     # 현재는 사용자가 말하는것을 그대로 답 하는 상태
     json_return = {
         "message": {
-            "text":msg
+            "text":return_msg
             
         },
         "keyboard": {
